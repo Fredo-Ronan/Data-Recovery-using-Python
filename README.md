f970e211a20f26803049684d576dd18b16b15389
Fredo-Ronan <fredocode06@gmail.com>
2024-01-22 22:27:24 +0700

    modify some code and readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Data Recovery using Python
 This repository is about data recovery using python script. This python script is designed to recover multiple files.
 
-## Important Note ‼️
+## ‼️ IMPORTANT NOTE ‼️
 _This script is not tested yet and it's still in early step of development, so stay tuned for next refactor and optimization on this python script._
+<br>
+DO NOT USE THIS SCRIPT TO RECOVER YOUR MEMORY CARD OR THUMB DRIVES, IT'S STILL NOT STABLE YET!
--- multiple_data_recovery.py
+++ multiple_data_recovery.py
@@ -7,6 +7,13 @@ drec = False            # Recovery mode
 rcvd = 0                # Recovered file ID
 
 def foundJPG():
+    global byte
+    global rcvd
+    global drec
+    global offs
+    global size
+    global fileD
+    # print("Finding JPG Files...");
     found = byte.find(b'\xff\xd8\xff\xe0\x00\x10\x4a\x46')
     if found >= 0:
         drec = True
@@ -28,6 +35,13 @@ def foundJPG():
             else: fileN.write(byte)
 
 def foundPNG():
+    global byte
+    global rcvd
+    global drec
+    global offs
+    global size
+    global fileD
+    # print("Finding PNG Files...");
     found = byte.find(b'\x89\x50\x4E\x47')
     if found >= 0:
         drec = True
@@ -40,7 +54,7 @@ def foundPNG():
             bfind = byte.find(b'\x49\x45\x4E\x44\xAE\x42\x60\x82')
             fileN.write(byte)
             if bfind >= 0:
-                fileN.write(byte[:bfind+2])
+                fileN.write(byte[:bfind+8])
                 fileD.seek((offs+1)*size)
                 print('==== Wrote PNG to location: ' + str(rcvd) + '.png ====\n')
                 drec = False
@@ -49,6 +63,13 @@ def foundPNG():
             else: fileN.write(byte)
 
 def foundPDF():
+    global byte
+    global rcvd
+    global drec
+    global offs
+    global size
+    global fileD
+    # print("Finding PDF Files...");
     found = byte.find(b'\x25\x50\x44\x46\x2D\x31\x2E\x37')
     if found >= 0:
         drec = True
@@ -70,6 +91,13 @@ def foundPDF():
             else: fileN.write(byte)
 
 def foundDOC():
+    global byte
+    global rcvd
+    global drec
+    global offs
+    global size
+    global fileD
+    # print("Finding DOC Files...");
     found = byte.find(b'\xD0\xCF\x11\xE0\xA1\xB1\x1A\xE1')
     if found >= 0:
         drec = True
@@ -82,7 +110,7 @@ def foundDOC():
             bfind = byte.find(b'\x4D\x69\x63\x72\x6F\x73\x6F\x66\x74\x20\x57\x6F\x72\x64\x20\x39\x37\x2D\x32\x30\x30\x33')
             fileN.write(byte)
             if bfind >= 0:
-                fileN.write(byte[:bfind+2])
+                fileN.write(byte[:bfind+22])
                 fileD.seek((offs+1)*size)
                 print('==== Wrote DOC to location: ' + str(rcvd) + '.doc ====\n')
                 drec = False
@@ -94,19 +122,47 @@ def foundDOCX():
     pass
 
 def foundMP4():
-    pass
+    global byte
+    global rcvd
+    global drec
+    global offs
+    global size
+    global fileD
+    # print("Finding MP4 Files...");
+    found = byte.find(b'\x00\x00\x00\x18\x66\x74\x79\x70\x6d\x70')
+    if found >= 0:
+        drec = True
+        print('==== Found MP4 at location: ' + str(hex(found+(size*offs))) + ' ====')
+        # Now lets create recovered file and search for ending signature
+        fileN = open(str(rcvd) + '.mp4', "wb")
+        fileN.write(byte[found:])
+        while drec:
+            byte = fileD.read(size)
+            bfind = byte.find(b'\x00\x00\x00\x18\x66\x74\x79\x70\x6d\x70')
+            fileN.write(byte)
+            if bfind >= 0:
+                fileN.write(byte[:bfind])
+                fileD.seek((offs+1)*size)
+                print('==== Wrote MP4 to location: ' + str(rcvd) + '.mp4 ====\n')
+                drec = False
+                rcvd += 1
+                fileN.close()
+            else: fileN.write(byte)
 
 def findAllSupportedFiles():
     foundJPG()
     foundPNG()
     foundPDF()
     foundDOC()
-    foundDOCX()
-    foundMP4()
+    # foundDOCX()
+    # foundMP4()
+
+print(f"Scanning disk drive {drive} and Finding JPG, PNG, PDF and DOC files...")
 
 while byte:
     findAllSupportedFiles()
     byte = fileD.read(size)
     offs += 1
 
+print(f"Scanning complete...")
 fileD.close()
